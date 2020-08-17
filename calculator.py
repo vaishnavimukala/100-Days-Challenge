1740b7f83d8d6f42855cba6fb2ec38142a340ec6
vaishnavimukala <52392339+vaishnavimukala@users.noreply.github.com>
2020-08-17 13:37:08 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,47 @@
+a=int(input("Enter a value:"))
+b=int(input("Enter b value:"))
+def calculator(x,y):
+    print(x,"+",y,"=",x+y)
+    print(x,"-",y,"=",x-y)
+    print(x,"*",y,"=",x*y)
+    print(x,"/",y,"=",x/y)
+    print(x,"%",y,"=",x%y)
+    print(x,"//",y,"=",x//y)
+    print(x,"power",y,"=",x**y)
+calculator(a,b)
+#-------------------------------
+#a=int(input("Enter a value:"))
+#b=int(input("Enter b value:"))
+#op=input("Enter the operation:")
+#def cal(x,y,oper):
+#   if oper=='+':
+#       return x+y
+#   elif oper=='-':
+#       return x-y
+#   elif oper=='*':
+#        return x*y
+#   elif oper=='/':
+#        return x/y
+#    elif oper=='%':
+#        return x%y
+#    else:
+#        return "invalid operator"
+#print(cal(a,b,op))    
+#----------------------------------
+#a=int(input("Enter a value:"))
+#b=int(input("Enter b value:"))
+#def calci(x,y):
+#   answer=[]
+#    answer.append(x+y)
+#    answer.append(x-y)
+#    answer.append(x*y)
+#    answer.append(x/y)
+#    return answer
+#print("Sum of ",a,"and",b,"is",calci(a,b)[0])
+#print("Difference of ",a,"and",b,"is",calci(a,b)[1])
+#print("Product of",a,"and",b,"is",calci(a,b)[2])
+#print("Division of ",a,"and",b,"is",calci(a,b)[3])
+#calci(a,b)
+    
+
+
